be4b72e2b4c9d820a644a13b5c1d818de52de55f
Tanmoy Sarkar <tnmsarkar96@gmail.com>
2020-04-13 12:01:36 +0530

    Add files via upload

---
+++ client/src/actions/errorActions.js
@@ -0,0 +1,18 @@
+import {GET_ERRORS, CLEAR_ERRORS } from './types';
+
+//REturn errors
+export const returnErrors = (msg, status, id = null) => {
+    return {
+        type: GET_ERRORS,
+        payload: {
+            msg, status,id
+        }
+    };
+};
+
+//Clear errors
+export const clearErrors = () => {
+    return {
+        type : CLEAR_ERRORS
+    };
+};
